482495580f0c72f511a0d049dfe8c7d49be494c5
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-17 15:00:08 +0900

    learn: result of Assignment1

--- assignment1/app.js
+++ assignment1/app.js
@@ -1,7 +1,44 @@
 const http = require('http');
 
-const routes = require('./routes');
+const server = http.createServer((req, res) => {
+  const url = req.url;
 
-const server = http.createServer(routes.handler);
+  if (url ===　'/') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>LEARNING NODE.JS</title></head>');
+    res.write('<div>hello welcome</div>')
+    res.write(
+      '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit"></button></form></body>'
+    );
+    res.write('</html>');
+
+    return res.end()
+  }
+
+
+  if (url === '/users') {
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('<head><title>LEARNING NODE.JS</title></head>');
+    res.write('<ul><li>User 1</li></ul>')
+    res.write('</html>');
+    return res.end();
+  }
+
+  if (url === '/create-user') {
+    const body = [];
+    req.on('data', (chunk) => {
+      body.push(chunk);
+    });
+    req.on('end', () => {
+      const parseBody = Buffer.concat(body).toString();
+      console.log(parseBody.split('=')[1]);
+    });
+    res.statusCode = 302;
+    res.setHeader('Location', '/');
+    res.end();
+  }
+})
 
 server.listen(3000);
