8517ae91dd68dba1bfcaef473b130b562df40943
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-22 02:22:22 +0900

    feat: filtering path /admin

--- routes/admin.routes.js
+++ routes/admin.routes.js
@@ -2,13 +2,15 @@ const express = require('express');
 
 const router = express.Router();
 
+// /admin/app-product => GET
 router.get('/add-product', (req, res, next) => {
   res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"></input><button type="submit">Add Product</button></form>'
+    '<form action="/admin/add-product" method="POST"><input type="text" name="title"></input><button type="submit">Add Product</button></form>'
   );
 });
 
-router.post('/product', (req, res, next) => {
+// /admin/app-product => POST
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
