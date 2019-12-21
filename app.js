68b43cf47908f5335e8d7d71807c39bb02e17909
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-21 17:44:37 +0900

    learn: learning Express, parsing incoming request

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-  console.log('This is always running💫');
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"></input><button type="submit">Add Product</button></form>'
+  );
 });
 
 app.use('/product', (req, res, next) => {
-  console.log('IN the next middleware❗');
-  res.send('<h1>PRODUCT PAGE</h1>');
+  console.log(req.body);
+  res.redirect('/');
 });
 
 app.use('/', (req, res, next) => {
-  console.log('IN the next middleware❗');
   res.send('<h1>Hello from Express</h1>');
 });
 
