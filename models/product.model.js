f8cd058dc1c6c050e690819aa15c981dd8eb6397
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-25 22:39:40 +0900

    feat: storage product data in json file

--- models/product.model.js
+++ models/product.model.js
@@ -1,4 +1,6 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../utils/path.util');
 
 module.exports = class Product {
   constructor(title) {
@@ -6,10 +8,27 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const p = path.join(rootDir, 'data', 'products.json');
+
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products))) err => {
+        console.log(err);
+      };
+    });
   }
 
   static fetchAll() {
-    return products;
+    const p = path.join(rootDir, 'data', 'products.json');
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+      return JSON.parse(fileContent);
+    });
   }
 };
