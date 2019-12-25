ed60c526c1fd2d74dd7a3e44a91b5ccde592cc00
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-25 19:15:06 +0900

    learn: context API

--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product.model');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('add-product', {
@@ -8,10 +8,12 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
+  const products = Product.fetchAll();
   res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' });
 };
