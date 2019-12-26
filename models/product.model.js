11cefa600772710d46578806adb03cc24de3a339
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-26 17:21:07 +0900

    refac: refactoring product model

--- models/product.model.js
+++ models/product.model.js
@@ -2,19 +2,25 @@ const fs = require('fs');
 const path = require('path');
 const rootDir = require('../utils/path.util');
 
+const p = path.join(rootDir, 'data', 'products.json');
+
+const getProductsFormFile = callback => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      callback([]);
+    } else {
+      callback(JSON.parse(fileContent));
+    }
+  });
+};
+
 module.exports = class Product {
   constructor(title) {
     this.title = title;
   }
 
   save() {
-    const p = path.join(rootDir, 'data', 'products.json');
-
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFormFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
@@ -23,12 +29,6 @@ module.exports = class Product {
   }
 
   static fetchAll(callback) {
-    const p = path.join(rootDir, 'data', 'products.json');
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        callback([]);
-      }
-      callback(JSON.parse(fileContent));
-    });
+    getProductsFormFile(callback);
   }
 };
