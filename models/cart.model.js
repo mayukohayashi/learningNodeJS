8c301010ccfd68588c8af9bf194938d9c56f9bdf
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-28 16:22:46 +0900

    feat&fix: set cart model & define setting of node versin etc on json

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,5 +1,27 @@
+const fs = require('fs');
+const path = require('path');
+const rootDir = require('../utils/path.util');
+
+const p = path.join(rootDir, 'data', 'cart.json');
+
 module.exports = class Cart {
   static addProduct(id) {
-    
+    // Fetch the previous cart
+    fs.readFile(p, (err, fileContent) => {
+      const cart = { products: [], totalPrice: 0 };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+
+      // Analze the cart => Find existing product
+      const existingProduct = cart.products.find(prod => prod.id === id);
+      let updatedProduct;
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty += 1;
+      }
+    });
+
+    // Add new product / increase quantity
   }
-}
+};
