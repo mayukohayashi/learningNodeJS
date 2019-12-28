02736802c34323f818ca24265eb807f922759633
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-28 18:31:01 +0900

    feat: store edited product into data

--- controllers/admin.controller.js
+++ controllers/admin.controller.js
@@ -14,7 +14,7 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
 
-  const product = new Product(title, imageUrl, price, description);
+  const product = new Product(null, title, imageUrl, price, description);
 
   product.save();
   res.redirect('/');
@@ -40,8 +40,21 @@ exports.getEditProduct = (req, res, next) => {
 };
 
 exports.postEditProduct = (req, res, next) => {
-  
-}
+  const prodId = req.body.productId;
+  const updatedTitle = req.body.title;
+  const updatedImageUrl = req.body.imageUrl;
+  const updatedPrice = req.body.price;
+  const updatedDescription = req.body.description;
+  const updatedProduct = new Product(
+    prodId,
+    updatedTitle,
+    updatedImageUrl,
+    updatedPrice,
+    updatedDescription
+  );
+  updatedProduct.save();
+  res.redirect('/admin/products')
+};
 
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
