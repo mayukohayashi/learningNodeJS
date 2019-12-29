8a366d11ddaa7bf6cdd33d3942159d4a708f7244
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-29 20:16:02 +0900

    learn: using sql, mysql database

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error.controller');
+const db = require('./utils/database')
 
 const app = express();
 
@@ -13,6 +14,8 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin.routes');
 const shopRoutes = require('./routes/shop.routes');
+
+db.execute('');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
