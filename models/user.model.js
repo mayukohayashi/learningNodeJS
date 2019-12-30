a16b9e97ea7f6264a0659e6a7edc97b0852d640d
mayukohayashi <mayuko.hayashi.spf.rd@gmail.com>
2019-12-30 18:21:05 +0900

    add: set cart feature to user model

--- models/user.model.js
+++ models/user.model.js
@@ -4,14 +4,30 @@ const getDb = require('../utils/database').getDb;
 const ObjectId = mongodb.ObjectId;
 
 class User {
-  constructor(username, email) {
+  constructor(username, email, cart, id) {
     this.name = username;
     this.email = email;
+    this.cart = cart;
+    this._id = id;
   }
 
   save() {
     const db = getDb();
     return db.collection('users').insertOne(this);
+  }
+
+  addToCart(product) {
+    // const cartProduct = this.cart.items.findIndex(cp => {
+    //   return cp._id === product._id;
+    // });
+    const updatedCart = { items: [{ ...product, quantity: 1 }] };
+    const db = getDb();
+    return db
+      .collection('users')
+      .updatedOne(
+        { _id: new ObjectId(this._id) },
+        { $set: { cart: updatedCart } }
+      );
   }
 
   static findById(userId) {
@@ -24,7 +40,7 @@ class User {
         return user;
       })
       .catch(err => {
-        console.log(err)
+        console.log(err);
       });
   }
 }
